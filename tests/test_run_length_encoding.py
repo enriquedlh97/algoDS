c3c64c45bf50b3feed5f57fd6d9424e5682f0e73
enriquedlh97 <enriquedlh97@hotmail.com>
2021-04-09 10:54:00 -0500

    Adds tests for test_run_length_encoding

--- tests/test_run_length_encoding.py
+++ tests/test_run_length_encoding.py
@@ -3,10 +3,10 @@ import sys
 
 import pytest
 
-from algosds.problems.patterns.pattern_sliding_window.caesar_cipher_encryptor import \
-    caesar_cipher_encryptor_hash, \
-    caesar_cipher_encryptor_hash_original, \
-    caesar_cipher_encryptor_alphabet_original
+from algosds.problems.patterns.pattern_sliding_window.run_length_encoding import \
+    run_length_encoding_single_for_loop, \
+    run_length_encoding_double_for_loop, \
+    run_length_encoding_original
 
 myPath = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, myPath + '/../')
@@ -21,9 +21,25 @@ tests = [("AAAAAAAAAAAAABBCCCCDD", "9A4A2B4C2D"),
          ("1  222 333    444  555", "112 321 334 342 35"),
          ("1A2BB3CCC4DDDD", "111A122B133C144D"),
          ("........______=========AAAA   AAABBBB   BBB", "8.6_9=4A3 3A4B3 3B"),
-         ("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "9a9a9a9a9a9a9a9a9a9a9a9a9a9a9a9a9a9a9a9a9a9a9a1a"),
+         ("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+          "9a9a9a9a9a9a9a9a9a9a9a9a9a9a9a9a9a9a9a9a9a9a9a1a"),
          ("        aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "8 9a9a9a9a9a4a"),
          (" ", "1 "),
          ("[(aaaaaaa,bbbbbbb,ccccc,dddddd)]", "1[1(7a1,7b1,5c1,6d1)1]"),
          (";;;;;;;;;;;;''''''''''''''''''''1233333332222211112222111s", "9;3;9'9'2'111273524142311s"),
          ("AAAAAAAAAAAAABBCCCCDDDDDDDDDDD", "9A4A2B4C9D2D")]
+
+
+@pytest.mark.parametrize("string, answer", tests)
+def test_run_length_encoding_single_for_loop(string, answer):
+    assert run_length_encoding_single_for_loop(string) == answer
+
+
+@pytest.mark.parametrize("string, answer", tests)
+def test_run_length_encoding_double_for_loop(string, answer):
+    assert run_length_encoding_double_for_loop(string) == answer
+
+
+@pytest.mark.parametrize("string, answer", tests)
+def test_run_length_encoding_original(string, answer):
+    assert run_length_encoding_original(string) == answer
