b81864a9e756eeb47f89ee96030bc589574d6791
enriquedlh97 <enriquedlh97@hotmail.com>
2021-04-14 12:20:10 -0500

    Extends documentation for nth fibonacci solutions

--- algosds/problems/patterns/miscellaneous/dynamic_programming_and_recursion/nth_fibonacci.py
+++ algosds/problems/patterns/miscellaneous/dynamic_programming_and_recursion/nth_fibonacci.py
@@ -20,6 +20,13 @@ Output:
 # Time O(2^n), where n is the nth fibonacci number (quantity of Fibonacci numbers that have to be computed)
 # Space O(n)
 def get_nth_fib_recursive(n):
+    """ Traditional recursive solution
+
+
+
+    :param n: integer representing the nth fibonacci number to be calculated
+    :return: integer representing the nth fibonacci number
+    """
     if n == 1:
         return 0
     elif n == 2:
@@ -31,6 +38,22 @@ def get_nth_fib_recursive(n):
 # Time O(n), where n is the nth fibonacci number (quantity of Fibonacci numbers that have to be computed)
 # Space O(1) if we assume that the compiler does tail call elimination, if not, then we have O(n) space
 def get_nth_fib_tail_recursive(n, two_previous=1, amount=0):
+    """ Tail recursive solution
+
+    This solution, as it calls its recursive part, in only takes O(n) time because it only calls itself once, it does
+    not split into two recursive calls. The recursive equation would be T(n) = T(n - 1) + 1. Also, since most modern
+    compilers are able to do call tail elimination, the space complexity if O(1).
+
+    More information on tail recursion adn tail call elimination is here:
+    https://www.programmersought.com/article/71146901590/
+    https://www.geeksforgeeks.org/tail-recursion/
+    https://www.geeksforgeeks.org/tail-call-elimination/
+
+    :param n: integer representing the nth fibonacci number to be calculated
+    :param two_previous:
+    :param amount:
+    :return: integer representing the nth fibonacci number
+    """
     if n == 1:
         return amount
     elif n == 2:
@@ -43,6 +66,14 @@ def get_nth_fib_tail_recursive(n, two_previous=1, amount=0):
 # Time O(n), where n is the nth fibonacci number (quantity of Fibonacci numbers that have to be computed)
 # Space O(1)
 def get_nth_fib_iterative_for_loop(n):
+    """ Iterative solution with for loop
+
+    Computes the nth fibonacci number only when the 3rd or higher nth fibonacci number is asked. It basically keeps
+    track of the previous two fibonacci numbers and sums them up to compute the next one.
+
+    :param n: integer representing the nth fibonacci number to be calculated
+    :return: integer representing the nth fibonacci number
+    """
     two_previous = 0
     one_previous = 1
 
@@ -51,6 +82,8 @@ def get_nth_fib_iterative_for_loop(n):
     elif n == 2:
         return one_previous
     else:
+
+        n_th_number = 0
 
         for i in range(3, n + 1):
             n_th_number = one_previous + two_previous
@@ -63,6 +96,27 @@ def get_nth_fib_iterative_for_loop(n):
 # Time O(n), where n is the nth fibonacci number (quantity of Fibonacci numbers that have to be computed)
 # Space O(n), because of the hashmap
 def get_nth_fib_memoization(n, memoize={1: 0, 2: 1}):
+    """ Uses memoization/caching of previous fibonacci numbers
+
+    This would be the next optimization after the traditional recursive solution. The traditional recursive solutions
+    calls the recursive function twice for every fibonacci number, this means that it essentially computes most of the
+    fibonacci numbers twice. For instance, if asked for fibonacci(n=8). It will call fibonacci(n=7) and fibonacci(n=6)
+    and then fibonacci(n=7) itself calls fibonacci(n=6) and fibonacci(n=5). And the previous fibonacci(n=6) from the
+    first call, calls fibonacci(n=5) and fibonacci(n=4). And the pattern continuous. This is very inefficient because
+    the same numbers are computed multiple times.
+
+    What this solution does is, for every fibonacci number that it computes, it keeps track of it in a hashtable. Every
+    time it checks if it is already in a hashtable with previously computed fibonacci numbers, if it is not, then it
+    calls itself again (recursive part) to get the previous two numbers (which are always already precomputed and is,
+    therefore, only a O(1) time operation) and compute the new fibonacci number.
+
+    For this reason, every fibonacci number is only computed once, making the algorithm a O(n) time and space because it
+    stores every single fibonacci number.
+
+    :param n: integer representing the nth fibonacci number to be calculated
+    :param memoize:
+    :return: integer representing the nth fibonacci number
+    """
     if n in memoize:
         return memoize[n]
     else:
@@ -73,6 +127,14 @@ def get_nth_fib_memoization(n, memoize={1: 0, 2: 1}):
 # Time O(n), where n is the nth fibonacci number (quantity of Fibonacci numbers that have to be computed)
 # Space O(1)
 def get_nth_fib_iterative_while(n):
+    """ Iterative solution with while loop
+
+    It is the same as the iterative solution with for loop. It basically keeps track of the previous two fibonacci
+    numbers and sums them up to compute the next one.
+
+    :param n: integer representing the nth fibonacci number to be calculated
+    :return: integer representing the nth fibonacci number
+    """
     last_two = [0, 1]
     counter = 3
     while counter <= n:
