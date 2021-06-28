0cc5725f8e55b4fe552c61ba3e172cf4ce895a32
enriquedlh97 <enriquedlh97@hotmail.com>
2021-06-28 16:55:57 -0500

    Adds problem description for move_elemnt_to_end.py

---
+++ algosds/problems/categories/arrays/move_element_to_end.py
@@ -0,0 +1,19 @@
+"""
+Problem:
+
+You're given an array of integers and an integer. Write a function that moves all instances of that integer in the array
+to the end of the array and returns the array.
+
+The function should perform this in place (i.e„ it should mutate the input array) and doesnt need to maintain the order
+of the other integers.
+
+Input:
+
+    array: [2, 1, 2, 2, 2, 3, 4, 2]
+    to_move: 2
+
+Output:
+
+    [4, 1, 3, 2, 2, 2, 2, 2]
+
+"""
