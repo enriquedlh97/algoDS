6e386edda5eda9b8bbd7399e895e6a87ed8e6b81
enriquedlh97 <enriquedlh97@hotmail.com>
2021-06-29 17:10:35 -0500

    Adds documention to solutions from move_element_to_end.py

--- algosds/problems/categories/arrays/move_element_to_end.py
+++ algosds/problems/categories/arrays/move_element_to_end.py
@@ -23,8 +23,23 @@ from collections import deque
 # Time O(n), where n is the length of the input array
 # Space O(1)
 def move_element_to_end_original(array, to_move):
+    """ Original solution
+
+    This solution works by initializing pointers to the beginning and end of the input array. The right pointer moves
+    elements that do not correspond to the indicated value in the variable to_move. The left pointers moves elements
+    that due correspond to the to_move value. Essentially, everytime both the left and right pointers find valid values
+    they perform a swap.
+
+    The execution is done until both pointers meet each other.
+
+    :param array: Non-empty array of integers
+    :param to_move: Integer corresponding to the values that have to be sent to the end of the original array
+    :return: the original array with values corresponding to the to_move value sent to the back
+    """
     i = 0
     j = len(array) - 1
+    # This first while loop, makes sure that the right pointer is moved all the way down until a valid number is found.
+    # This is done to prevent iterating over non-valid values.
     while i < j:
         while i < j and array[j] == to_move:
             j -= 1
@@ -37,6 +52,15 @@ def move_element_to_end_original(array, to_move):
 # Time O(n), where n is the length of the input array
 # Space O(1)
 def move_element_to_end_list(array, to_move):
+    """ My solution, very similar to the original one
+
+    This solution works basically the same as the previous original solution. The only exception is that there is no
+    outer while loop.
+
+    :param array: Non-empty array of integers
+    :param to_move: Integer corresponding to the values that have to be sent to the end of the original array
+    :return: the original array with values corresponding to the to_move value sent to the back
+    """
     # Initialize pointers
     left = 0
     right = len(array) - 1
@@ -58,8 +82,21 @@ def move_element_to_end_list(array, to_move):
 
 
 # Time O(n), where n is the length of the input array
-# Space O(1)
+# Space O(n)
 def move_element_to_end_list_deque(array, to_move):
+    """ My solution using deque
+
+    This solution is very similar to the previous two solutions. The difference is that a for loop is used to iterate
+    over the input array and the values that do not correspond to to_move values are popped out of the array and saved
+    to a temporary one. The values that due correspond to the to_move value are popped and appended to the original
+    array. At the end, the temporary array containing the non-valid values is prepended to the original array.
+
+    This solution yields a higher space complexity, which in the worst case scenario will be O( n - 1 ) = O(n)
+
+    :param array: Non-empty array of integers
+    :param to_move: Integer corresponding to the values that have to be sent to the end of the original array
+    :return: the original array with values corresponding to the to_move value sent to the back
+    """
     # Initialize temporary array for saving non-key elements
     temporary_array = []
 
@@ -77,4 +114,3 @@ def move_element_to_end_list_deque(array, to_move):
         new_array.appendleft(x)
 
     return list(new_array)
-
