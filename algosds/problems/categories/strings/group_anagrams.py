17ba9cf284ff75e9242ce84c15e093a0a1ae3766
enriquedlh97 <enriquedlh97@hotmail.com>
2021-07-02 16:26:30 -0500

    Extends documentation for solutions in group_anagram.py

--- algosds/problems/categories/strings/group_anagrams.py
+++ algosds/problems/categories/strings/group_anagrams.py
@@ -50,14 +50,65 @@ def group_anagrams_hash_table(words):
 def group_anagrams_original_brute_force(words):
     """ Original brute force solution
 
+    Because of how this solution is structures, it has to first handle for the case when the input array of words is
+    empty.
+
+    This solution works by first arranging in alphabetical order the letters in each word from the input array of words.
+    This is done immediately after the array of words is received. It corresponds to the first step.
+
+    To do this, the algorithm iterates over each of the words. Then sorts the letters of each word in alphabetical order
+    and appends the sorted word into a new array containing the sorted words. This operation takes O(w) time to loop
+    over each word (w is the number of words in the input array) and n*lg(n) time for sorting the letters of each word
+    (where n is the length of the longest word). So, the whole operation takes O(w * n * lg(n)) time. Additionally,
+    since a new array of sorted words of length w (where w is the number of words in the input array) the operation
+    takes O(w) space.
+
+    This operation has the following effect. Assuming an input array
+
+    words: ["yo", "act", "flop", "tac", "foo", "cat", "oy", "olfp"]
+
+    The resulting sorted array would be as follows:
+
+    sorted_array: ["oy", "act", "flop", "act", "foo", "act", "oy", "flop"]
+
+    Notice how the order of the words is the same, the only thing that changes is the order in the letters for each
+    word.
+
+    The next is to create an array of indices and sort it according to how the previous array of sorted words would be
+    if sorted.
+
+    So, the sorted array with its corresponding indices would be as follows.
+
+    sorted_array: ["oy", "act", "flop", "act", "foo", "act", "oy", "flop"]
+         indices    0      1       2      3     4       5      6     7
+
+    Now, if the sorted_array was sorted, that is, setting the order of the words within the array in alphabetical order,
+    the resulting array, along with the correspond indices would look as follows.
+
+    sorted_array: ["act", "act", "act", "flop", "flop", "foo", "oy", "oy"]
+         indices    0      1       2      3     4       5      6     7
+
+    Nevertheless, we do not want to actually sort the sorted_array, that is why the array of indices is created.
+
     ::param words: Input array of strings containing potential anagrams
     :return: List of anagram groups in no particular order
     """
     if len(words) == 0:
         return []
 
+    # This step loops over each word from the input array of words, sorts the letter alphabetically for each word and
+    # then appends each sorted word to a new array of sorted words. Here, sorted(w) returns an array with the letters of
+    # the word sorted in alphabetical order.  For example, sorted('hola') -> ['a', 'h', 'l', 'o']. For this reason, and
+    # to get back a string, the "".join() method is used to concatenate the characters into a string. So, in this case
+    # "".join(sorted('hola')) -> 'ahlo'.
+    # This operation takes O(w * n * lg(n)) time and O(w) space
     sorted_words = ["".join(sorted(w)) for w in words]
+    # This creates an array of size w (where w is the number of words in the input array) with vales from 0 to w - 1
     indices = [i for i in range(len(words))]
+    # This operation sorts the elements in the indices array, corresponding to the positions of the words in the
+    # sorted_array, in the same order that they would be in if the actual words that they represent were sorted in
+    # alphabetical order.
+    # The operation takes O(w * lg(w))
     indices.sort(key=lambda x: sorted_words[x])
 
     result = []
