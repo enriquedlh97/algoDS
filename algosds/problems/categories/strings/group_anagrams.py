681238cfd2c20ddea86e57d81ec034db7a70e95d
enriquedlh97 <enriquedlh97@hotmail.com>
2021-07-03 17:34:18 -0500

    Adds documentation for brute force solution in group_anagrams.py

--- algosds/problems/categories/strings/group_anagrams.py
+++ algosds/problems/categories/strings/group_anagrams.py
@@ -45,23 +45,23 @@ def group_anagrams_hash_table(words):
     return [anagram_group for anagram_group in strings_dict.values()]
 
 
-# Time: O(w*n*lg(n) + n*w*lg(w))
+# Time: O(w*n*lg(n) + n*w*lg(w)) time, where w is the number of words and n the length of the longest word
 # Space: O(w*n)
 def group_anagrams_original_brute_force(words):
     """ Original brute force solution
 
-    Because of how this solution is structures, it has to first handle for the case when the input array of words is
+    Because of how this solution is structured, it has to first handle for the case when the input array of words is
     empty.
 
-    This solution works by first arranging in alphabetical order the letters in each word from the input array of words.
-    This is done immediately after the array of words is received. It corresponds to the first step.
+    Then, this solution works by first arranging in alphabetical order the letters in each word from the input array of
+    words. This is done immediately after the array of words is received.
 
-    To do this, the algorithm iterates over each of the words. Then sorts the letters of each word in alphabetical order
-    and appends the sorted word into a new array containing the sorted words. This operation takes O(w) time to loop
-    over each word (w is the number of words in the input array) and n*lg(n) time for sorting the letters of each word
-    (where n is the length of the longest word). So, the whole operation takes O(w * n * lg(n)) time. Additionally,
-    since a new array of sorted words of length w (where w is the number of words in the input array) the operation
-    takes O(w * n) space. The n is because it corresponds to the length of the longest word.
+    To do this, the algorithm iterates over each of the words. Then, it sorts the letters of each word in alphabetical
+    order and appends the sorted word into a new array containing the sorted words. This operation takes O(w) time to
+    loop over each word (w is the number of words in the input array) and O(n*lg(n)) time for sorting the letters of
+    each word (where n is the length of the longest word). So, the whole operation takes O(w * n * lg(n)) time.
+    Additionally, since a new array of sorted words of length w (where w is the number of words in the input array) is
+    created, the operation takes O(w * n) space. The n is because it corresponds to the length of the longest word.
 
     This operation has the following effect. Assuming an input array
 
@@ -71,24 +71,48 @@ def group_anagrams_original_brute_force(words):
 
     sorted_array: ["oy", "act", "flop", "act", "foo", "act", "oy", "flop"]
 
-    Notice how the order of the words is the same, the only thing that changes is the order in the letters for each
+    Notice how the order of the words is the same, the only thing that changes is the order of the letters for each
     word.
 
-    The next is to create an array of indices and sort it according to how the previous array of sorted words would be
-    if sorted.
+    The next step is to create an array of indices and sort it according to how the previous array of sorted words would
+    be if sorted with respect to the words.
 
     So, the sorted array with its corresponding indices would be as follows.
 
-    sorted_array: ["oy", "act", "flop", "act", "foo", "act", "oy", "flop"]
-         indices    0      1       2      3     4       5      6     7
+    sorted_array: ["oy", "act", "flop", "act", "act", "oy", "flop"]
+         indices    0      1       2      3     4       5      6
 
     Now, if the sorted_array was sorted, that is, setting the order of the words within the array in alphabetical order,
     the resulting array, along with the correspond indices would look as follows.
 
-    sorted_array: ["act", "act", "act", "flop", "flop", "foo", "oy", "oy"]
-         indices    0      1       2      3     4       5      6     7
+    sorted_array: ["act", "act", "act", "flop", "flop", "oy", "oy"]
+         indices    1      3       4      2       6      0      5
 
     Nevertheless, we do not want to actually sort the sorted_array, that is why the array of indices is created.
+
+    This operation of sorting the array of indices according to how the sorted_Array would end up if sorted takes
+    O( n * w * lg(w) ) time. Sorting the array of indices takes O( w * lg(w) ) time because there are w indices in the
+    array. The n in O( n * w * lg(w) ) comes from the fact that we are dealing with strings. So, the operation is
+    essentially sorting an array of strings and not an array of integers. If the operation was to sort an array of
+    integers it would take only O( w * lg(w) ), because for w indices we perform at most w comparisons. Nevertheless, in
+    the case of strings, in order to compare two strings, in the worst case, all of the characters of the strings have
+    to be compared, and this would yield an O(n) time operation, where n is the length of the longest word.
+
+    Essentially, this operation of comparing two strings is equivalent to comparing two arrays.
+
+    In summary, the operation involves sorting the w elements, which takes O(w * lg(w)) time and for each element, for
+    each of the O( w * lg(w) ) comparisons, we have to compare each character of the string, this comparison takes O(n)
+    time. This means that the whole operation takes O( n * w * lg(w) ) time.
+
+    To read more on sorting strings, string comparisons and the reason for the time complexity in this operation see the
+    following links:
+
+    https://stackoverflow.com/a/57062358/15542477
+    https://stackoverflow.com/questions/34318489/is-it-true-that-sorting-strings-is-on2logn/34318565
+    https://cs.stackexchange.com/questions/127899/what-is-the-expected-time-complexity-of-checking-equality-of-two-arbitrary-strin
+
+    As a whole, the algorithm ends up taking O(w*n*lg(n) + n*w*lg(w)) time, where w is the number of words and n the
+    length of the longest word
 
     ::param words: Input array of strings containing potential anagrams
     :return: List of anagram groups in no particular order
@@ -109,7 +133,25 @@ def group_anagrams_original_brute_force(words):
     # sorted_array, in the same order that they would be in if the actual words that they represent were sorted in
     # alphabetical order.
     # The operation takes O(n * w * lg(w))
+    # The key value is to sort the elements according to the string that the index represents in the sorted array.
+    #
+    # The value of the key parameter should be a function (or other callable) that takes a single argument and returns a
+    # key to use for sorting purposes. This technique is fast because the key function is called exactly once for each
+    # input record.
+    # A common pattern is to sort complex objects using some of the object’s indices as keys. For example:
+    # For example,
+    # >>> student_tuples = [
+    # ...    ('john', 'A', 15),
+    # ...    ('jane', 'B', 12),
+    # ...    ('dave', 'B', 10),
+    # ...]
+    # >>> sorted(student_tuples, key=lambda student: student[2])   # sort by age
+    # [('dave', 'B', 10), ('jane', 'B', 12), ('john', 'A', 15)]
+    # For more details ont his, see https://docs.python.org/3/howto/sorting.html#key-functions
     indices.sort(key=lambda x: sorted_words[x])
+    # For details on lambda use see https://www.w3schools.com/python/python_lambda.asp
+    # lambda x: sorted_words[x] is equivalent to writing
+    # x = lambda x: sorted_words[x] and the x function is called as x(0) which is equivalent to sorted_words[0]
 
     result = []
     current_anagram_group = []
@@ -119,6 +161,7 @@ def group_anagrams_original_brute_force(words):
         word = words[index]
         sorted_word = sorted_words[index]
 
+        # This operation of comparing two strings takes O(n) time.
         if sorted_word == current_anagram:
             current_anagram_group.append(word)
             continue
