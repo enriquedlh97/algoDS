2f403f44bb331c3dbd744209390583a6c2261d6e
enriquedlh97 <enriquedlh97@hotmail.com>
2021-07-08 16:13:36 -0500

    Adds linked list file with classes

---
+++ algosds/data_structures/linked_list.py
@@ -0,0 +1,18 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        pass
+
+
+if __name__ == "__main__":
+    linked_list = LinkedList
+    linked_list.append('A')
+    linked_list.append('B')
--- algosds/data_structures/structures.py
+++ algosds/data_structures/structures.py
@@ -186,3 +186,5 @@ class Deque:
         :return:
         """
         return self.items[-1]
+
+    
