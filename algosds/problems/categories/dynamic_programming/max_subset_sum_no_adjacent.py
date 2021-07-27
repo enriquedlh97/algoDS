6fdd82d3cf9d1490042ece18c3acd100cd151d2a
enriquedlh97 <enriquedlh97@hotmail.com>
2021-07-27 09:59:24 -0500

    Fixes bug

--- algosds/problems/categories/dynamic_programming/max_subset_sum_no_adjacent.py
+++ algosds/problems/categories/dynamic_programming/max_subset_sum_no_adjacent.py
@@ -89,7 +89,7 @@ def max_subset_sum_no_adjacent_brute_force_original(array):
     if not len(array):
         return 0
     elif len(array) == 1:
-        return array[1]
+        return array[0]
     max_sums = array[:]
     max_sums[1] = max(array[0], array[1])
     for i in range(2, len(array)):
